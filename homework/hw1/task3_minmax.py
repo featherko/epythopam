b68931df5478a824f537995749e91c2d43283544
featherko <featherko@gmail.com>
2021-03-08 10:42:08 +0300

    Homework 1 + tests for homework1 with fixes

--- homework/hw1/task3_minmax.py
+++ homework/hw1/task3_minmax.py
@@ -21,26 +21,6 @@ def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
     :param file_name: Input file
     :return: : Min, Max
     """
-    """seq = []
-    with open("input.txt") as fi:
-        for line in fi:
-            seq.append(line)
-    return min(seq), max(seq)"""
-
-    """
-    #with open("input.txt") as fi:
-        lines = fi.readlines()
-        min_value = int(lines[0])
-        max_value = int(lines[0])
-        for line in lines:
-            line = int(line)
-            if line > max_value:
-                max_value = line
-            elif line < min_value:
-                min_value = line
-        return min_value, max_value
-    """
-
     with open(file_name) as fi:
         lines = fi.readlines()
         lines = list(map(int, lines))
--- homework/hw1/task4_sum41.py
+++ homework/hw1/task4_sum41.py
@@ -8,16 +8,6 @@ We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
 import itertools
 from typing import List
 
-"""def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
-    answer = 0
-    for i in a:
-        for j in b:
-            for k in c:
-                for l in d:
-                    if a[i] + b[j] + c[k] + d[l] == 0:
-                        answer += 1
-    return answer"""
-
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
     """Check sum 4.
@@ -31,19 +21,3 @@ def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) ->
     :return: Number of such tuples
     """
     return sum(1 for x in itertools.product(a, b, c, d) if sum(x) == 0)
-
-
-"""def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
-    counter = 0
-    sum1 = {}
-    for i in a:
-        for j in b:
-            if i + j not in sum1:
-                sum1[i + j] = 1
-            else:
-                sum1[i + j] += 1
-    for i in c:
-        for j in d:
-            if -1 * (i + j) in sum1:
-                counter += sum1[-1 * (i + j)]
-    return counter"""
